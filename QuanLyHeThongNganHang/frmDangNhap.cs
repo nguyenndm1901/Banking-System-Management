204870ed96527326c93fb4f13409a4a3cfbee042
Nguyễn Đức Minh Nguyên <43044839+nguyenndm1901@users.noreply.github.com>
2020-11-05 00:32:03 +0700

    Create frmDangNhap.cs

---
+++ QuanLyHeThongNganHang/frmDangNhap.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyHeThongNganHang
+{
+    public partial class frmDangNhap : Form
+    {
+        public frmDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void labelForgot_Click(object sender, EventArgs e)
+        {
+            Label labelForgot = sender as Label;
+
+            if (labelForgot != null)
+            {
+                // If the clicked label is black, the player clicked
+                // an icon that's already been revealed --
+                // ignore the click
+                if (labelForgot.ForeColor == Color.Black)
+                    return;
+
+                labelForgot.ForeColor = Color.Black;
+            }
+        }
+    }
+}
