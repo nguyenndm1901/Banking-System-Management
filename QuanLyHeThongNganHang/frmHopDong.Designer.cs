5a83cfcaf23e1f10f49c299238b406f97585e65e
Nguyễn Đức Minh Nguyên <43044839+nguyenndm1901@users.noreply.github.com>
2020-12-11 13:49:03 +0700

    Update

--- QuanLyHeThongNganHang/frmHopDong.Designer.cs
+++ QuanLyHeThongNganHang/frmHopDong.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(667, 332);
+            this.ClientSize = new System.Drawing.Size(869, 616);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "frmHopDong";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
